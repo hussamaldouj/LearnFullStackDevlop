6c1d0823cac3bf54ffae47c5672bbc491c4234d3
hussamaldouj <54702124+hussamaldouj@users.noreply.github.com>
2022-04-10 13:58:35 +0300

    Add files via upload

---
+++ js/vendors/code-editor/code-editor-active.js
@@ -0,0 +1,11 @@
+(function ($) {
+ "use strict";
+
+		var editor_one = CodeMirror.fromTextArea(document.getElementById("code1"), {
+					 lineNumbers: true,
+					 matchBrackets: true,
+					 styleActiveLine: true,
+					 theme:"ambiance"
+				 });
+ 
+})(jQuery); 
